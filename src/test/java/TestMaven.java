9936d8c781ee65dee6fd086de7ee7b09622f221d
i031723 <netravathi.mahadevaiah@sap.com>
2019-04-30 14:50:46 +0200

    first commit

---
+++ src/test/java/TestMaven.java
@@ -0,0 +1,12 @@
+
+
+
+import org.testng.annotations.Test;
+
+public class TestMaven {
+	 @Test
+	    public void exampleOfTestNgMaven() {
+	        System.out.println("This is TestNG-Maven Example");
+	    }
+}
+
